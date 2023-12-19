345900275041da719bad11fe18ffbfd23f1b303f
Irisu <nekomarisakirisame@gmail.com>
2023-12-19 17:35:02 -0500

    Add chat event queue

---
+++ Kumi.Server/Queues/ChatEvent.cs
@@ -0,0 +1,30 @@
+﻿using Kumi.Game.Online.API.Accounts;
+using Kumi.Game.Online.API.Chat;
+using Kumi.Server.Processing;
+using Newtonsoft.Json;
+
+namespace Kumi.Server.Queues;
+
+public class ChatEvent : QueueItem<ChatEvent.ChatEventData>
+{
+    public class ChatEventData 
+    {
+        /// <summary>
+        /// The type of event sent.
+        /// </summary>
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        
+        /// <summary>
+        /// The channel this event is associated with.
+        /// </summary>
+        [JsonProperty("channel")]
+        public APIChatChannel Channel { get; set; }
+        
+        /// <summary>
+        /// The account that sent the event.
+        /// </summary>
+        [JsonProperty("account")]
+        public APIAccount Account { get; set; } 
+    }
+}
---
+++ Kumi.Server/Queues/ChatQueue.cs
@@ -0,0 +1,37 @@
+﻿using Kumi.Game.Online.Server.Packets.Dispatch;
+using Kumi.Server.Processing;
+
+namespace Kumi.Server.Queues;
+
+public partial class ChatQueue : QueueProcessor
+{
+    [Queue("chat:events")]
+    public async Task ProcessChatEvent(ChatEvent chatEvent)
+    {
+        switch (chatEvent.Data.Type)
+        {
+            case "join":
+                onJoin(chatEvent.Data);
+                break;
+        }
+    }
+    
+    private void onJoin(ChatEvent.ChatEventData data)
+    {
+        // Find the account that joined the channel, if they exist.
+        var connections = WebsocketServer.GetConnections(c => c.Account.Id == data.Account.Id);
+        
+        if (connections.Count() == 0)
+            return;
+        
+        var joinPacket = new ChatChannelAddEvent()
+        {
+            Data = new ChatChannelAddEvent.ChatChannelAddEventData()
+            {
+                Channel = data.Channel
+            }
+        };
+        
+        connections.Send(joinPacket);
+    }
+}
